6b74cb44b08ebb154b5b94cde81b07313b5de48c
Jbosco339 <benjaminadakole20@gmail.com>
2026-01-29 03:55:32 +0100

    worked on the home-hero component and made color changes

--- src/components/Home_Hero_Section.jsx.jsx
+++ src/components/Home_Hero_Section.jsx.jsx
@@ -6,12 +6,13 @@ function HeroSection() {
         <section id='section1'>
           <article id='article1'>
             <div id='first_div'>
-              {/* <h1>Words that Matter.</h1>
+              <div id='live_edit'><p>LIVE EDIT V2.0</p></div>
+              <h1>Words that Matter.</h1>
               <p>
                 The modern standard for digital publishing. Connect with a global audience through beautiful, focused typography.
               </p>
               <button>Start Reading</button>
-              <button>Become a Writer</button> */}
+              <button>Become a Writer</button>
             </div>
             <div id='second_div'>2</div>
           </article>
