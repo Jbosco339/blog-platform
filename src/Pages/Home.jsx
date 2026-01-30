8c97a0c4a1a85f200c99464a9011bde595d8b2d9
Jbosco339 <benjaminadakole20@gmail.com>
2026-01-30 10:06:38 +0100

    finished the hero page footer and styling

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import HeroSection from '../components/Home_Hero_Section.jsx'
 import Percentage_Section from '../components/Percentage_Section.jsx'
+import Footer from '../components/Footer.jsx'
 
 function Home() {
   return (
     <div>
         <HeroSection/>
         <Percentage_Section/>
+        {/* <Footer/> */}
     </div>
   )
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div></div>
+    <footer id='footer'>
+      <div id='footer_div'>
+        <div id='footer_inner_div'>
+          <h1 id='dont_miss'>Don't miss a single beat.</h1>
+        <p id='join_developers'>Join 15,000+ developers receiving our weekly curated digest.</p>
+        <input type="text" placeholder='you@example.com' id='input_text'/>
+        <button id='button_footer'>Join Now</button>
+        </div>
+      </div>
+    </footer>
   )
 }
 
